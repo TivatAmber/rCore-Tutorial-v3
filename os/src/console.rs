8b618ef859a6ce4b6f7efa79a31a8ffc12875588
TivatAmber <TivatAmber@gmail.com>
2023-01-19 03:48:20 +0800

    Log

--- os/src/console.rs
+++ os/src/console.rs
@@ -33,3 +33,43 @@ macro_rules! println {
         $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
     }
 }
+
+/// error macro
+#[macro_export]
+macro_rules! error {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat("\x1b[31m", "[Error] ", $fmt, "\x1b[0m", "\n") $(, $(arg)+)?));
+    };
+}
+
+/// warn macro
+#[macro_export]
+macro_rules! warn {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat("\x1b[93m", "[WARN] ", $fmt, "\x1b[0m", "\n") $(, $(arg)+)?));
+    }
+}
+
+/// info macro
+#[macro_export]
+macro_rules! info {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!("\x1b[34m", "[INFO] ", $fmt, "\x1b[0m", "\n") $(, $($arg)+)?));
+    }
+}
+
+/// debug macro
+#[macro_export]
+macro_rules! debug {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!("\x1b[32m", "[DEBUG] ", $fmt, "\x1b[0m", "\n") $(, $($arg)+)?));
+    }
+}
+
+/// trace macro
+#[macro_export]
+macro_rules! trace {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!("\x1b[32m", "[TRACE] ", $fmt, "\x1b[0m", "\n") $(, $($arg)+)?));
+    }
+}
